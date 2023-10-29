47d838496dbcf4d959d5b332b07e31b3c4833f4d
Box <me@boxz.dev>
2023-10-29 09:36:18 +0000

    update

--- Assignment2/scripts/translate.sh
+++ Assignment2/scripts/translate.sh
@@ -7,3 +7,14 @@
 < /mnt/iwslt2014zh-en/test/test.translated.en
 
 # BLEU = 83.98, 95.6/86.4/80.8/76.4 (BP=0.994, ratio=0.994, hyp_len=18386, ref_len=18496)
+
+for i in {1..19}
+do
+/home/moses/mosesdecoder/bin/moses -f /mnt/fine-tune/run$i.moses.ini \
+--output-unknowns \
+< /mnt/iwslt2014zh-en/test/test.token.clean.50.zh > /mnt/iwslt2014zh-en/test/test.translated.$i.en
+
+/home/moses/mosesdecoder/scripts/generic/multi-bleu.perl \
+-lc /mnt/iwslt2014zh-en/test/test.token.clean.50.en \
+< /mnt/iwslt2014zh-en/test/test.translated.$i.en
+done
